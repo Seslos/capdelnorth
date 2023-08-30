3e1bcf63e2f29af91bd21cf63c66e7ad122df31b
Seslos <51689703+Seslos@users.noreply.github.com>
2023-08-30 09:08:04 -0400

    Update CourseController.php

--- app/Http/Controllers/Instructor/CourseController.php
+++ app/Http/Controllers/Instructor/CourseController.php
@@ -43,7 +43,7 @@ class CourseController extends Controller{
         $course = Course::create($request->all());
     
         if ($request->file('file')) {
-            $url= Storage::put('/public/storage/courses',$request->file('file'));
+                   $url= base64_encode($request->file('file'));
             $course->image()->create([
                 'url'=>$url
             ]);
