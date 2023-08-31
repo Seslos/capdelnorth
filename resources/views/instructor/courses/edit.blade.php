3b8c1e7d3caf3c2889ec0398ceabcba14f19d7a7
Seslos <51689703+Seslos@users.noreply.github.com>
2023-08-31 08:53:49 -0400

    Update edit.blade.php

--- resources/views/instructor/courses/edit.blade.php
+++ resources/views/instructor/courses/edit.blade.php
@@ -1,15 +1,10 @@
-
-
 <x-instructor-layout :course="$course">
-
     <h1 class="text-2x1 font-bold">INFORMACIÓN DEL CURSO</h1>
     <hr class="mt-2 mb-6">
     {!! Form::model($course, ['route'=>['instructor.courses.update',$course],'method'=>'put','files' =>true]) !!}
     @include('instructor.courses.partials.form')
-
                         <div class="flex justify-end">
-                            {!! Form::submit('Actualizar Información', ['class'=>'submit submit-primary']) !!}
-
+                    {!! Form::submit('Actualizar Información', ['class'=>'bg-indigo-600 shadow-xl hover:bg-indigo-500 text-white font-bold rounded-full p-4 w-48']) !!}
                         </div>
     {!! Form::close() !!}
 <x-slot name="js">
