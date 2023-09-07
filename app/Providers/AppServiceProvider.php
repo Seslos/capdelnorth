90b4065f7a04a51df6fbd350947bea006892469a
Seslos <51689703+Seslos@users.noreply.github.com>
2023-09-07 10:21:03 -0400

    i

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
- if(config('app.debug')!=true) {
-    \URL::forceScheme('https');
-  }
+        \URL::forceScheme('https');
         Lesson::observe(LessonObserver::class);
         Section::observe(SectionObserver::class);
         Blade::directive('routeIs', function ($expression) {
