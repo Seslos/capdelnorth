06f1f8a1f5a5526dee318b77417e118c9afdb71b
Seslos <51689703+Seslos@users.noreply.github.com>
2023-09-07 10:07:44 -0400

    kk

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -15,7 +15,7 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (!$request->secure() && App::environment() === 'production'){
+        if (!$request->secure() && App::environment() === 'capdelnorth'){
             return redirect()->secure($request->getRequestUri());
         }
 
