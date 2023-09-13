b58085f1b8eb5680e9717db49ac0ca3b92cd0b05
Seslos <51689703+Seslos@users.noreply.github.com>
2023-09-13 13:25:21 -0400

    ff

--- resources/views/livewire/instructor/lesson-resources.blade.php
+++ resources/views/livewire/instructor/lesson-resources.blade.php
@@ -17,7 +17,7 @@
         @else
             <form wire:submit.prevent="save">
                 <div class="flex items-center">
-                    <input wire:model="file" type="file" class="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" >
+                    <input wire:model="file" type="file" name="url" class="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" >
                     
                     <button type="submit" class="focus:outline-none text-white bg-blue-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-red-900 ml-2">Guardar</button>
                 </div>
